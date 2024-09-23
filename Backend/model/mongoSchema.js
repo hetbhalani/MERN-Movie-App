392947b6432cee7d1d063b389ff3e52cd34eec56
hetbhalani <bhalanihet2006@gmail.com>
2024-09-23 13:18:06 +0530

    add schema

---
+++ Backend/model/mongoSchema.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://bhalanihet2006:login@login.izxtn.mongodb.net/")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema);
+
+module.exports = collection
