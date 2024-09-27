2a92b457d3f0cd96b6328fdaa386788f8cdd3cf0
hetbhalani <bhalanihet2006@gmail.com>
2024-09-28 00:43:00 +0530

    add new schema

---
+++ Backend/model/mongoSchema.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://bhalanihet2006:login@login.izxtn.mongodb.net/Login?retryWrites=true&w=majority")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+const newSchema = mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+const user = mongoose.model('user',newSchema)
+export default user;
