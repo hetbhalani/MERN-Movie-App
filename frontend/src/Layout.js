5f6f6ab6add518825f3548bfa11944943d4f2e31
hetbhalani <bhalanihet2006@gmail.com>
2024-09-27 22:53:18 +0530

    change layout

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Outlet, Link, useNavigate } from 'react-router-dom';
-import { useSearch } from './SearchContext';
+import React from "react";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import { useSearch } from "./SearchContext";
 
 function Layout() {
   const { searchTerm, setSearchTerm } = useSearch();
@@ -8,22 +8,49 @@ function Layout() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate('/card'); // Navigate to home page where Card component is rendered
+    navigate("/card");
   };
 
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid d-flex justify-content-between align-items-center">
-          <img src="/imgs/logo.jpg" style={{ width: '100px' }} alt="Logo" />
+          
+          <img src="/imgs/logo.jpg" style={{ width: "100px" }} alt="Logo" />
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                className="nav-link active ms-2"
+                aria-current="page"
+                to="/"
+                style={{ fontSize: "18px" }}
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active ms-2"
+                aria-current="page"
+                to="/watchlist"
+                style={{ fontSize: "18px" }}
+              >
+                Watchlist
+              </Link>
+            </li>
+          </ul>
           <div className="flex-grow-1 text-center">
-            <form className="d-flex justify-content-center" role="search" onSubmit={handleSubmit}>
+            <form
+              className="d-flex justify-content-center"
+              role="search"
+              onSubmit={handleSubmit}
+            >
               <input
                 className="form-control me-1"
                 type="search"
-                placeholder="Search"
+                placeholder="Search For Movies/Series"
                 aria-label="Search"
-                style={{ width: '700px' }}
+                style={{ width: "700px" }}
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
@@ -35,17 +62,20 @@ function Layout() {
           <div className="d-flex">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/" style={{ fontSize: '18px' }}>
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link active" to="/login" style={{ fontSize: '18px' }}>
+                <Link
+                  className="btn btn-outline-primary me-3 ms-2 btn-md"
+                  to="/login"
+                  style={{ fontSize: "18px" }}
+                >
                   LogIn
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link active" to="/signup" style={{ fontSize: '18px' }}>
+                <Link
+                  className="btn btn-primary btn-md"
+                  to="/signup"
+                  style={{ fontSize: "18px" }}
+                >
                   SignUp
                 </Link>
               </li>
@@ -61,4 +91,4 @@ function Layout() {
   );
 }
 
-export default Layout
+export default Layout;
