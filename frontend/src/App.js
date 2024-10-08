1cf1245994d9f0cfb90565730190a47bc4838a8b
hetbhalani <bhalanihet2006@gmail.com>
2024-10-08 10:20:48 +0530

    default app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
-import Layout from "./Layout";
-import SearchResults from "./SearchResults";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  // Handle search value submission from Layout
-  const handleSearchSubmit = (value) => {
-    setSearchQuery(value);
-  };
-
   return (
-    <>
-      <Layout />
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
