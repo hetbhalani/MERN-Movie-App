9439ea6b00a3cf972f3b198113fae5593373d01e
hetbhalani <bhalanihet2006@gmail.com>
2024-10-08 09:50:03 +0530

    change start msg

--- Backend/model/mongoSchema.js
+++ Backend/model/mongoSchema.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
-    console.log("mongodb connected");
+    console.log("login db connected");
 })
 .catch(()=>{
     console.log('failed');
